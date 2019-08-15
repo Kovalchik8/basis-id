336fc7bfee12df9f8b0be129d5dd5e28a36b99fd
Kovalchk <koval4ik8@email.ua>
2019-08-15 17:00:40 +0300

    page about us desktop markup

---
+++ components/component-form.php
@@ -0,0 +1,48 @@
+<section class="form">
+  <div class="container-wrapper">
+    <h3 class="headline align-center headline--white">Be up and running in minutes</h3>
+    <span class="headline-text align-center">Questions? Schedule some time to talk with one of our experts.</span>
+
+    <div class="container">
+
+      <form>
+        <div class="form-group">
+          <label>Your Name <span>*</span></label>
+          <input type="text" class="form-control" placeholder="Type your name">
+        </div>
+        <div class="form-group">
+          <label>Your Email <span>*</span></label>
+          <input type="email" class="form-control" placeholder="Type your email">
+        </div>
+        <div class="form-group">
+          <label>Your Phone <span>*</span></label>
+          <input type="tel" class="form-control" placeholder="Type your Phone">
+        </div>
+        <div class="form-group">
+          <label>Your Company <span>*</span></label>
+          <input type="text" class="form-control" placeholder="Type your company">
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlSelect1">Which industry or type of activity you have?</label>
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option disabled selected>Select your industry type</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Your Message</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"
+            placeholder="Write what’s important for you we’ll find the best solution"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn--pink">Send your request</button>
+        <span class="note">By submitting this form, you agree to the Privacy Policy of our website.</span>
+      </form>
+
+    </div>
+  </div>
+</section>
